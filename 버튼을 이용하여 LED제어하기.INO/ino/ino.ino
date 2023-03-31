f528c3c9af0e0c31be33339311fe90198d2c0d9a
황병규 <128976098+banggi0323@users.noreply.github.com>
2023-03-31 13:27:25 +0900

    Create ino.ino

---
+++ 버튼을 이용하여 LED제어하기.INO/ino/ino.ino
@@ -0,0 +1,16 @@
+int buttonPin = 4;
+int ledPin = 13;
+int buttonState = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+void loop(){
+  buttonState = digitalRead(buttonPin);
+   if (buttonState == HIGH) { 
+     digitalWrite(ledPin, HIGH);
+   } 
+   else {
+     digitalWrite(ledPin, LOW);
+   }
+}
