e2e88a475257565dab7d31a8b2773f2b3db8aed4
황병규 <128976098+banggi0323@users.noreply.github.com>
2023-04-10 17:27:12 +0900

    Update ino.ino

--- 세그먼트 회로결선/ino/ino.ino
+++ 세그먼트 회로결선/ino/ino.ino
@@ -1,35 +1,37 @@
+byte Numbers[16][8] = {
+{1,1,1,1,1,1,0,0}, // 0
+{0,1,1,0,0,0,0,0}, // 1
+{1,1,0,1,1,0,1,0}, // 2
+{1,1,1,1,0,0,1,0}, // 3
+{0,1,1,0,0,1,1,0}, // 4
+{1,0,1,1,0,1,1,0}, // 5
+{1,0,1,1,1,1,1,0}, // 6
+{1,1,1,0,0,1,0,0}, // 7
+{1,1,1,1,1,1,1,0}, // 8
+{1,1,1,1,0,1,1,0}, // 9
+{1,1,1,0,1,1,1,0}, // A
+{0,0,1,1,1,1,1,0}, // B
+{1,0,0,1,1,1,0,0}, // C
+{0,1,1,1,1,0,1,0}, // D
+{1,0,0,1,1,1,1,0}, // E
+{1,0,0,0,1,1,1,0}  // F
+};
+
+int pin = 2;
 int num = 0;
-byte numbers[10]= {0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE4, 0xFE, 0xF6};
-void displayNumber(int n){
-  int pin = 2;
-  for(int i = 0; i < 8; i++) {
-    boolean state = bitRead(numbers[n], 7-i);
-    digitalWrite(pin+i, state);
-    }
-}
 
 void setup() {
-  pinMode(12, INPUT);
-  pinMode(13, INPUT);
-  for (int i = 2; i <= 9; i++) {
-    pinMode(i, OUTPUT);
+ for (int i = 2; i <= 9; i++) {
+        pinMode(i, OUTPUT);
     }
-    digitalWrite(9, HIGH);
+    digitalWrite(9, HIGH); 
 }
 
 void loop() {
-  if (digitalRead(12) == HIGH){
-    ++num;
-    if (num > 9) { 
-      num = 0;
-      }
+  for(int i = 0; i < 8; i++) {
+        digitalWrite(pin+i, Numbers[num][i]);
     }
-  if (digitalRead(13) == HIGH){
-    --num;
-    if (num < 0) {
-      num = 9;
-    }
-  }
-  displayNumber(num);
-  delay(300);
+    num++;
+    if (num > 9) num = 0;
+    delay(1000);
 }
